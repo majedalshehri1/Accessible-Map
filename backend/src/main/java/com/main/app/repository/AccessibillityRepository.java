24aa63820f367f7db55ce7cf143d688fdbbc25d4
majedalshehri1 <engmajedas1@gmail.com>
2025-08-04 03:16:18 -0700

    Create new repository accessibillity and review

---
+++ backend/src/main/java/com/main/app/repository/AccessibillityRepository.java
@@ -0,0 +1,9 @@
+package com.main.app.repository;
+
+import com.main.app.model.Accessibillity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccessibillityRepository extends JpaRepository<Accessibillity, Integer> {
+}
