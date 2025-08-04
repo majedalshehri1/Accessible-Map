c06b15ca50167375fab4ab5b85842ef3b21b4e7d
Abdulaziz H Shibrayn <ah.shibrayn@wakeb.tech>
2025-08-04 05:10:14 -0700

    Add Place Service and Enum Category and update Place Repo

--- backend/src/main/java/com/main/app/repository/PlaceRepository.java
+++ backend/src/main/java/com/main/app/repository/PlaceRepository.java
@@ -1,10 +1,15 @@
 package com.main.app.repository;
 
 
+import com.main.app.Enum.Category;
 import com.main.app.model.Place;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface PlaceRepository extends JpaRepository<Place,Integer> {
+public interface PlaceRepository extends JpaRepository<Place,Integer>{
+
+    List<Place> getCategory(Category category);
 }
