7f34e88fe4247e0e13245f28fb7ad491f3374fb6
= <=>
2025-08-04 00:03:57 -0700

    Test

--- backend/src/main/java/com/main/app/model/Review.java
+++ backend/src/main/java/com/main/app/model/Review.java
@@ -1,4 +1,19 @@
 package com.main.app.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Builder
+@Table(name = "review",
+        indexes = @Index(name = "idx_token_user", columnList = "user_id"))
 public class Review {
 }
