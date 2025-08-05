b6820078b18efca3983824c8dd4095b46c672ac7
ah.shibrayn <abdulaziz.binshibrayn@gmail.com>
2025-08-05 02:30:39 -0700

    Update Sec Config

--- backend/src/main/java/com/main/app/config/SecurityConfig.java
+++ backend/src/main/java/com/main/app/config/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**","/api/place/all/**").permitAll()
+                        .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/api/users/username").authenticated()
                         .anyRequest().authenticated()
                 )
