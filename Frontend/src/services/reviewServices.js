21bacb8d85739fd55288fef9830736d8b03b6055
majedalshehri1 <engmajedas1@gmail.com>
2025-08-05 23:51:51 -0700

    Install axios API

---
+++ Frontend/src/services/reviewServices.js
@@ -0,0 +1,10 @@
+import axiosClient from "@/apis/axiosClient";
+
+export default {
+    getReviewsByPlaceId(placeId) {
+        return axiosClient.get(`/reviews/place/${placeId}`);
+    },
+    createReview(reviewData) {
+        return axiosClient.post("/reviews", reviewData);
+    }
+}
---
+++ Frontend/src/services/userServices.js
@@ -0,0 +1,5 @@
+import axiosClient from "@/apis/axiosClient";
+
+export default{
+    
+}
