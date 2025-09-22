f7267becf5a6af451f2832539084ea1fa3345022
ah.shibrayn <abdulaziz.binshibrayn@gmail.com>
2025-09-21 23:03:34 -0700

    SSL sertificate and Nginx

--- Frontend/src/apis/axiosClient.js
+++ Frontend/src/apis/axiosClient.js
@@ -1,6 +1,11 @@
 import axios from "axios";
+
+// const BASE = process.env.VITE_API_BASE_URL;
+const BASE = import.meta.env.VITE_API_BASE_URL
+
+
 const api = axios.create({
-  baseURL: "/api",         
+  baseURL: `${BASE}/api`,
   withCredentials: true,
   headers: { Accept: "application/json", "Content-Type": "application/json" },
 });
